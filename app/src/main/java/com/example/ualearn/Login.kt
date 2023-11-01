2dc2b86f863f986e2e8038f3d5c40edd54adc7df
Lholi7 <142983465+Lholi7@users.noreply.github.com>
2023-11-01 03:12:53 +0200

    updates

--- app/src/main/java/com/example/ualearn/Login.kt
+++ app/src/main/java/com/example/ualearn/Login.kt
@@ -3,16 +3,46 @@ package com.example.ualearn
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
+    private val auth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+        val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
+        val loginButton = findViewById<Button>(R.id.loginButton)
         val signUpTextView = findViewById<TextView>(R.id.signUpTextView)
+
+        // Handle the login button click
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Login successful, navigate to MainActivity
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        // Login failed, display an error message
+                        val errorMessage = task.exception?.message
+                        Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+
+        // Navigate to the Register page
         signUpTextView.setOnClickListener {
-            val intent = Intent(this, Login::class.java)
+            val intent = Intent(this, Register::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/ualearn/Register.kt
+++ app/src/main/java/com/example/ualearn/Register.kt
@@ -3,14 +3,44 @@ package com.example.ualearn
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
+import com.google.firebase.auth.FirebaseAuth
 class Register : AppCompatActivity() {
+
+    private val auth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+        val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
+        val signUpButton = findViewById<Button>(R.id.signUpButton)
         val loginTextView = findViewById<TextView>(R.id.LoginTextView)
+
+        // Handle the sign-up button click
+        signUpButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Registration successful, navigate to MainActivity
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        // Registration failed, display an error message
+                        val errorMessage = task.exception?.message
+                        Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+
+        // Navigate to the Login page
         loginTextView.setOnClickListener {
             val intent = Intent(this, Login::class.java)
             startActivity(intent)
